76709f60767114da83b98dfb1174b4531a2c9897
Hidayet Takcı <37986378+htakci@users.noreply.github.com>
2025-11-29 14:37:10 +0300

    Add date_difference.c to calculate date difference

---
+++ date_difference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int baslangic, bitis;
+printf(" Baslangic Tarihi Giriniz :");
+scanf("%d",&baslangic);
+printf(" \n Bitis Tarihi Giriniz :");
+scanf("%d",&bitis);
+printf("\n Iki tarih arası fark = %d",bitis-baslangic);
+return(0);
+}
