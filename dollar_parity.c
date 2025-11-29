099d7c807d6a08b2947bee95285de177d0056649
Hidayet Takcı <37986378+htakci@users.noreply.github.com>
2025-11-29 14:38:29 +0300

    Add dollar to euro parity calculation program

---
+++ dollar_parity.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ float dolar, euro;
+ printf(" TL cinsinden Dolar degerini giriniz :");
+ scanf("%f",&dolar);
+ printf(" TL cinsinden Euro degerini giriniz :");
+ scanf("%f",&euro);
+ printf(" \n Dolar / Euro paritesi =%f ",dolar/euro);
+return(0);
+}
