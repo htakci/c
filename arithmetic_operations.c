52875a793b2bd9d970bacecb69574177bc48cd08
Hidayet Takcı <37986378+htakci@users.noreply.github.com>
2025-11-29 14:35:55 +0300

    Implement basic arithmetic operations in C

---
+++ arithmetic_operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int x,y;
+printf(" Klavyeden iki sayi giriniz :");
+scanf("%d%d",&x,&x);
+printf("\n x+y=%d",x+y);
+printf("\n x-y=%d",x-y);
+printf("\n x*y=%d",x*y);
+printf("\n x/y=%f",(x*1.0)/y);
+return(0);
+}
