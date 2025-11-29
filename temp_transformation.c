faa6ea1e98397eb40b1d9cd2541143ad14c9731e
Hidayet Takcı <37986378+htakci@users.noreply.github.com>
2025-11-29 14:46:45 +0300

    Add temperature conversion from Fahrenheit to Celsius

---
+++ temp_transformation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ float fahrenheit, celsius;
+printf(" Fahrenheit türünde sıcaklık değerini giriniz :");
+scanf("%f",&fahrenheit);
+printf(" \n Celsius türünden sıcaklık değeri =%f ",(fahrenheit-32)/1.8);
+return(0);
+}
