9261cec283a7e91e3a328596cd25c464623d563b
Hidayet Takcı <37986378+htakci@users.noreply.github.com>
2025-11-29 15:13:40 +0300

    Implement rectangular area calculation
    Fix syntax errors and improve output prompts.

---
+++ rectangular_area.c
@@ -0,0 +1,12 @@
+#include<iostream.h> 
+void main() 
+{ 
+int uzun, kısa, alan; 
+cout<<"uzun kenarı giriniz: \n"; 
+cin>>uzun; 
+cout<<"kısa kenarı giriniz: \n"; 
+cin>>kısa 
+alan = uzun*kısa 
+cout<<"alan:"<<alan; 
+return 0; 
+}
